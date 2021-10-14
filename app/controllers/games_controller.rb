3340c846c498f4e9cb8ea3af20d7f00f87c0297a
Shane Nicholson <shanescottnicholson@gmail.com>
2021-10-14 10:17:21 -0700

    Adds name_game method to controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,14 @@
 class GamesController < ApplicationController
+  def name
+    name = params["my_name"]
+    if name[0] == "a"
+      render json: {message: "Hey #{name.upcase}, your name starts with the first letter of the alphabet!"}
+    else
+      render json: {message: "#{name.upcase}"}
+    end
+  end
+
+  def number
+    
+  end
 end
