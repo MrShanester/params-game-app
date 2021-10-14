3334f3f85e86eb4849279d0cb5e238c3639dc6f7
Shane Nicholson <shanescottnicholson@gmail.com>
2021-10-14 11:16:53 -0700

    Bugfixes url_number method to properly add together two variables

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -22,8 +22,8 @@ class GamesController < ApplicationController
   end
 
   def url_number
-    num1 = params["wildcard"]
-    num2 = params["wildcard2"]
+    num1 = params["wildcard"].to_i
+    num2 = params["wildcard2"].to_i
     num = num1 + num2
     if num.to_i == 36
       render json: {message: "You Guessed Right! The number was 36."}
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/name_game" => "games#name"
   get "/number_game" => "games#number"
-  get "/villian_game/:wildcard/:wildcard2" => "games#villian"
-  get "/url_number_game/:wildcard" => "games#url_number"
+  get "/villian_game/:wildcard" => "games#villian"
+  get "/url_number_game/:wildcard/:wildcard2" => "games#url_number"
   get "/hero_game/:wildcard" => "games#hero"
   get "bank_sim/:wildcard" => "games#bank"
 end
